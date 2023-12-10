f1eae5d6e7b6351106dbe3decfda7c49056a9813
Bernard_jung <bernardjung88@gmail.com>
2023-12-11 03:31:14 +0900

    로그인 페이지 완성 / 기타 수정 완료

--- js/main.js
+++ js/main.js
@@ -38,41 +38,6 @@ fadeEls.forEach(function (fadeEl, index) {
 });
 
 
-new Swiper('.notice-line .swiper-container', {
-    direction: 'vertical',
-    autoplay: true,
-    loop: true
-});
-
-new Swiper('.promotion .swiper-container', {
-    slidesPerView: 3,
-    spaceBetween: 10,
-    centeredSlides: true,
-    loop: true,
-    autoplay: {
-        delay: 5000
-    },
-    pagination: {
-        el: '.promotion .swiper-pagination',
-        clickable: true
-    },
-    navigation: {
-        prevEl: '.promotion .swiper-prev',
-        nextEl: '.promotion .swiper-next'
-    }
-});
-
-
-new Swiper('.awards .swiper-container', {
-    autoplay: true,
-    loop: true,
-    spaceBetween: 30,
-    slidesPerView: 4,
-    navigation: {
-        prevEl: '.awards .swiper-prev',
-        nextEl: '.awards .swiper-next'
-    }
-});
 
 const promotionEl = document.querySelector('.promotion');
 const promotionToggleBtn = document.querySelector('.toggle-promotion');
